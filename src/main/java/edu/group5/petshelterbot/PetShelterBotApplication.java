f71b5fa8e58c48d9cd018fcf7cb020a1eefab364
Иван <ivanpaskhin@yandex.ru>
2023-05-30 21:06:28 +0300

    Основная ветка для разработки

--- src/main/java/edu/group5/petshelterbot/PetShelterBotApplication.java
+++ src/main/java/edu/group5/petshelterbot/PetShelterBotApplication.java
@@ -2,8 +2,11 @@ package edu.group5.petshelterbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
+@EnableScheduling
 public class PetShelterBotApplication {
 
     public static void main(String[] args) {
