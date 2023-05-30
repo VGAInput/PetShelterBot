a8284b133c4dbd8d9ab79a69f9bcdb73a58631a8
Иван <ivanpaskhin@yandex.ru>
2023-05-30 20:41:52 +0300

    Initial commit

---
+++ src/main/java/edu/group5/petshelterbot/PetShelterBotApplication.java
@@ -0,0 +1,13 @@
+package edu.group5.petshelterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShelterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetShelterBotApplication.class, args);
+    }
+
+}
