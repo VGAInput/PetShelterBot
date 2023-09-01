f02051d676add9518151f248f0471e8da6e8d6cf
Melekhin Mikhail <melekhinmikhail14@gmail.com>
2023-09-01 23:51:28 +0300

    Добавлены тесты для сервиса волонтеров

---
+++ src/test/java/edu/group5/petshelterbot/service/VolunteerServiceTest.java
@@ -0,0 +1,140 @@
+package edu.group5.petshelterbot.service;
+
+import edu.group5.petshelterbot.entity.Volunteer;
+import edu.group5.petshelterbot.repository.VolunteerRepository;
+import org.apache.el.stream.Optional;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.internal.bytebuddy.dynamic.DynamicType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.postgresql.hostchooser.HostRequirement.any;
+
+@ExtendWith(MockitoExtension.class)
+class VolunteerServiceTest {
+
+    private static final String VOLUNTEER_NAME = "Volunteer";
+    private static final String SHELTER_TABLE_NAME = "CATS";
+    private static final Long TG_USER_ID = 1L;
+    private static final Integer IS_READY = 1;
+
+    private final Volunteer volunteer = new Volunteer(VOLUNTEER_NAME, SHELTER_TABLE_NAME, TG_USER_ID, IS_READY);
+
+    private static final List<Volunteer> volunteers = new ArrayList<>(Arrays.asList(
+            new Volunteer(VOLUNTEER_NAME, SHELTER_TABLE_NAME, TG_USER_ID, IS_READY),
+            new Volunteer(VOLUNTEER_NAME, SHELTER_TABLE_NAME, TG_USER_ID, IS_READY),
+            new Volunteer(VOLUNTEER_NAME, SHELTER_TABLE_NAME, TG_USER_ID, IS_READY)
+    ));
+
+    private static final List<Long> longs = new ArrayList<>(Arrays.asList(1L, 2L, 3L, 4L, 5L));
+
+    @Mock
+    private VolunteerRepository volunteerRepositoryMock;
+
+    @InjectMocks
+    private VolunteerService volunteerService;
+
+    @Test
+    void saveVolunteer() {
+        Mockito.when(volunteerRepositoryMock.save(any(Volunteer.class))).thenReturn(volunteer);
+
+        Volunteer volunteer1 = volunteerService.saveVolunteer(volunteer);
+
+        Assertions.assertThat(volunteer.getName()).isEqualTo(volunteer1.getName());
+        Assertions.assertThat(volunteer.getShelterTableName()).isEqualTo(volunteer1.getShelterTableName());
+        Assertions.assertThat(volunteer.getTgUserId()).isEqualTo(volunteer1.getTgUserId());
+        Assertions.assertThat(volunteer.getIsReady()).isEqualTo(volunteer1.getIsReady());
+    }
+
+    @Test
+    void getVolunteerByID() {
+        Mockito.when(volunteerRepositoryMock.findVolunteersById(any(Long.class))).thenReturn(volunteer);
+
+        Volunteer volunteer1 = volunteerService.getVolunteerByID(1L);
+
+        Assertions.assertThat(volunteer.getName()).isEqualTo(volunteer1.getName());
+        Assertions.assertThat(volunteer.getShelterTableName()).isEqualTo(volunteer1.getShelterTableName());
+        Assertions.assertThat(volunteer.getTgUserId()).isEqualTo(volunteer1.getTgUserId());
+        Assertions.assertThat(volunteer.getIsReady()).isEqualTo(volunteer1.getIsReady());
+    }
+
+    @Test
+    void getAllVolunteers() {
+        Mockito.when(volunteerRepositoryMock.findAll()).thenReturn(volunteers);
+
+        List<Volunteer> volunteers1 = volunteerService.getAllVolunteers();
+
+        Assertions.assertThat(volunteers1.size()).isEqualTo(volunteers.size());
+        Assertions.assertThat(volunteers1).isEqualTo(volunteers);
+    }
+
+    @Test
+    void checkVolunteerExists() {
+        Mockito.when(volunteerRepositoryMock.existsByShelterTableNameAndTgUserId(any(String.class), any(Long.class)))
+                .thenReturn(true);
+
+        boolean isExist = volunteerService.checkVolunteerExists(SHELTER_TABLE_NAME, TG_USER_ID);
+
+        Assertions.assertThat(isExist).isTrue();
+    }
+
+    @Test
+    void getAllVolunteersFromShelter() {
+        Mockito.when(volunteerRepositoryMock.findVolunteersByShelterTableName(any(String.class))).thenReturn(volunteers);
+
+        List<Volunteer> volunteers1 = volunteerService.getAllVolunteersFromShelter(SHELTER_TABLE_NAME);
+
+        Assertions.assertThat(volunteers1.size()).isEqualTo(volunteers.size());
+        Assertions.assertThat(volunteers1).isEqualTo(volunteers);
+    }
+
+    @Test
+    void volunteerIds() {
+        Mockito.when(volunteerRepositoryMock.getVolunteersFromShelter(any(String.class))).thenReturn(longs);
+
+        List<Long> volunteers1 = volunteerService.volunteerIds(SHELTER_TABLE_NAME);
+
+        Assertions.assertThat(volunteers1.size()).isEqualTo(longs.size());
+        Assertions.assertThat(volunteers1).isEqualTo(longs);
+    }
+
+    @Test
+    void getRandomVolunteer() {
+        Mockito.when(volunteerRepositoryMock.getVolunteersFromShelter(any(String.class))).thenReturn(longs);
+
+        List<Long> volunteers1 = volunteerService.volunteerIds(SHELTER_TABLE_NAME);
+
+        Assertions.assertThat(volunteers1.get(0)).isEqualTo(1L);
+    }
+
+    @Test
+    void isCurrentUserVolunteer() {
+    }
+
+    @Test
+    void isReady() {
+    }
+
+    @Test
+    void getVolunteerByTgUserId() {
+    }
+
+    @Test
+    void updateVolunteer() {
+    }
+
+    @Test
+    void deleteVolunteer() {
+    }
+}
